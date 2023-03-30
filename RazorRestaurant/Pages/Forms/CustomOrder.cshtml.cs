1e5a0b4e71ed5923bbff99cf461c2a888cfd367f
Zyadachi <ozdowy.adam8@gmail.com>
2023-03-30 22:34:35 +0200

    adding classes to uprage the look of text and images

---
+++ RazorRestaurant/Pages/Forms/CustomOrder.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorRestaurant.Pages.Shared.Forms
+{
+    public class CustomOrderModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
