60c8f1d85df61311036ac39c490daacf709986ac
Zyadachi <ozdowy.adam8@gmail.com>
2023-03-31 23:17:31 +0200

    Adding more functions to "Create your own pizza".

---
+++ RazorRestaurant/Models/PizzasModel.cs
@@ -0,0 +1,20 @@
+﻿namespace RazorRestaurant.Models
+{
+    public class PizzasModel
+    {
+        public string ImageTitle { get; set; }
+        public string PizzaName { get; set; }
+        public float BasePrice { get; set; } = 2;
+        public bool TomatoSauce { get; set; }
+        public bool Ham { get; set; }
+        public bool ExtraCheese { get; set; }
+        public bool Mushrooms { get; set; }
+        public bool Olives { get; set; }
+        public bool Pepperoni { get; set; }
+        public bool Jalapeno { get; set; }
+        public bool Salami { get; set; }
+        public bool Pineapple { get; set; }
+        public float FinalPrice { get; set; }
+
+    }
+}
--- RazorRestaurant/Pages/Forms/CustomPizza.cshtml.cs
+++ RazorRestaurant/Pages/Forms/CustomPizza.cshtml.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorRestaurant.Models;
 
 namespace RazorRestaurant.Pages.Forms
 {
     public class CustomPizzaModel : PageModel
     {
+        [BindProperty]
+        public PizzasModel Pizza { get; set; }
         public void OnGet()
         {
         }
