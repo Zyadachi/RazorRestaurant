35c3f23e86971e1bbba96d05c48ba31ebe4baff6
Zyadachi <ozdowy.adam8@gmail.com>
2023-04-01 17:12:02 +0200

    adding values to extras in pizza creator

--- RazorRestaurant/Pages/Forms/CustomPizza.cshtml.cs
+++ RazorRestaurant/Pages/Forms/CustomPizza.cshtml.cs
@@ -8,8 +8,23 @@ namespace RazorRestaurant.Pages.Forms
     {
         [BindProperty]
         public PizzasModel Pizza { get; set; }
+        public float PizzaPrice { get; set; }
         public void OnGet()
         {
+        }
+        public IActionResult onPost()
+        {
+            PizzaPrice = Pizza.BasePrice;
+            if (Pizza.TomatoSauce) PizzaPrice += 1;
+			if (Pizza.ExtraCheese) PizzaPrice += 1;
+			if (Pizza.Ham) PizzaPrice += 1;
+			if (Pizza.Mushrooms) PizzaPrice += 1;
+			if (Pizza.Olives) PizzaPrice += 1;
+			if (Pizza.Jalapeno) PizzaPrice += 1;
+			if (Pizza.Pepperoni) PizzaPrice += 1;
+			if (Pizza.Pineapple) PizzaPrice += 1;
+            return RedirectToPage("/Checkout/Checkout", new {Pizza.PizzaName, PizzaPrice});
+
         }
     }
 }
