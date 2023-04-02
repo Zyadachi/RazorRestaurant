e38d621b9f23f03867f7fc4fd88284316bceb562
Zyadachi <ozdowy.adam8@gmail.com>
2023-04-02 10:39:10 +0200

    adding SQL database to save information about Pizza Orders

---
+++ RazorRestaurant/Data/AplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorRestaurant.Models;
+
+namespace RazorRestaurant.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public DbSet<PizzaOrder> PizzaOrders { get; set; }
+
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) 
+            : base(options)
+        {
+
+        }
+    }
+}
